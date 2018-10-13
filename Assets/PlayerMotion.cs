22211c19fd45c3a9d7c4d51847ec5c6c2c49ceb0
ryuhei001 <31504472+ryuhei001@users.noreply.github.com>
2018-10-14 00:20:50 +0900

    original

---
+++ Assets/PlayerMotion.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMotion : MonoBehaviour {
+
+	private Animator ani;
+	// Use this for initialization
+	void Start () {
+		ani = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxis ("Vertical") > 0) {
+			ani.SetInteger ("Vertical", 1);
+
+		} else {
+			ani.SetInteger ("Vertical",0);
+		}
+		ani.SetBool ("Jump", Input.GetButton ("Jump"));
+	
+	}
+}
