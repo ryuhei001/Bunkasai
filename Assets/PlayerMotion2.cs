7cc2ca8ceffcd305b83d0bfaf17731d47dc5c715
ryuhei001 <31504472+ryuhei001@users.noreply.github.com>
2018-10-30 21:11:39 +0900

    player*4

---
+++ Assets/PlayerMotion2.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMotion2 : MonoBehaviour {
+    public GameObject bomb;
+
+	private Animator ani;
+	// Use this for initialization
+	void Start () {
+		ani = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetAxis ("Vertical2") > 0) {
+			ani.SetInteger ("Vertical", 1);
+
+		} else {
+			ani.SetInteger ("Vertical",0);
+		}
+		ani.SetBool ("Jump", Input.GetButton ("Jump"));
+        //射撃処理
+        if (Input.GetMouseButtonUp(0))
+        {
+            GameObject newBomb = Instantiate(bomb, new Vector3(transform.position.x, transform.position.y + 4.93f, transform.position.z), transform.rotation);
+            newBomb.GetComponent<BombScript>().SetVelocity(transform.forward);
+        }
+	}
+}
