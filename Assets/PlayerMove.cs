358c37041e044f4d91aad5b5eb790733717baa2f
iuwata <iuwata0701@gmail.com>
2018-10-31 00:46:12 +0900

    1
    first

--- Assets/PlayerMove.cs
+++ Assets/PlayerMove.cs
@@ -14,10 +14,10 @@ public class PlayerMove : MonoBehaviour {
 	public Transform rayPos;
 	public float rayRan = 0.85f;
 	public bool isGround;
+    private Transform mainCam;
 	void Start () {
 		anim = GetComponent<Animator> ();
-
-
+        mainCam = transform.Find("Main Camera");
 	}
 	
 	// Update is called once per frame
@@ -38,6 +38,25 @@ public class PlayerMove : MonoBehaviour {
 			moveDir = transform.TransformDirection (moveDir);
 			moveDir *= speed;
 			transform.Rotate (0, Input.GetAxis ("Horizontal") * rotSpeed, 0);
+            //float rotateX = (transform.eulerAngles.x > 180) ? transform.eulerAngles.x - 360 : transform.eulerAngles.x;
+            //float angleX = Mathf.Clamp(rotateX + Input.GetAxis("Vertical") * rotSpeed * -1, minAngle, maxAngle);
+            //angleX = (angleX < 0) ? angleX + 360 : angleX;
+            //float eulerAngleX = Mathf.Clamp(mainCam.eulerAngles.x + Input.GetAxis("Vertical"), minAngle, maxAngle);
+            Vector3 angles = mainCam.eulerAngles;
+            Debug.Log(mainCam.eulerAngles.x);
+            if(angles.x > 180 && angles.x < 340 && Input.GetAxis("Vertical") > 0)
+            {
+                angles = new Vector3(340, angles.y, angles.z);
+            }
+            else if(angles.x <= 180 && angles.x > 20 && Input.GetAxis("Vertical") <= 0)
+            {
+                angles = new Vector3(20, angles.y, angles.z);
+            }
+            else
+            {
+                mainCam.eulerAngles = new Vector3(angles.x + Input.GetAxis("Vertical") * rotSpeed * -1, angles.y, angles.z);
+            }
+            //transform.RotateAround(transform.position, transform.right, Input.GetAxis("Horizontal") * rotSpeed);
 
 			if (Input.GetButton ("Jump")) {
 				
@@ -49,6 +68,10 @@ public class PlayerMove : MonoBehaviour {
 		} else {
 			Debug.Log ("false");
 		}
+
+        
+
+
 		moveDir.y -= gra * Time.deltaTime;
 		chCon.Move (moveDir*Time.deltaTime);
 	}
